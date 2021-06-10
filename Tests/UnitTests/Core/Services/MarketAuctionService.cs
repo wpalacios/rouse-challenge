b8c5d5b7c8214bbe60d66ad647f238cd0e2e76e1
William Palacios <wfpmartinez@gmail.com>
2021-06-10 09:53:57 -0600

    Add small improvements to unit tests

--- Tests/UnitTests/Core/Services/MarketAuctionService.cs
+++ Tests/UnitTests/Core/Services/MarketAuctionService.cs
@@ -24,57 +24,57 @@
                             {
                                 Schedule = new Schedule()
                                 {
-                                    DefaultAuctionRatio = 0.02M,
-                                    DefaultMarketRatio = 0.002M,
+                                    DefaultAuctionRatio = 0.02m,
+                                    DefaultMarketRatio = 0.002m,
                                     Years = new Dictionary<string, Year>()
                                     {
                                         {
                                             "2006", new Year()
                                             {
-                                                AuctionRatio = 0.181383M,
-                                                MarketRatio = 0.311276M,
+                                                AuctionRatio = 0.181383m,
+                                                MarketRatio = 0.311276m,
                                             }
                                         },
                                         {
                                             "2007", new Year()
                                             {
-                                                AuctionRatio = 0.185085M,
-                                                MarketRatio = 0.317628M,
+                                                AuctionRatio = 0.185085m,
+                                                MarketRatio = 0.317628m,
                                             }
                                         },
                                         {
                                             "2008", new Year()
                                             {
-                                                AuctionRatio = 0.188862M,
-                                                MarketRatio = 0.324111M,
+                                                AuctionRatio = 0.188862m,
+                                                MarketRatio = 0.324111m,
                                             }
                                         },
                                         {
                                             "2009", new Year()
                                             {
-                                                AuctionRatio = 0.192716M,
-                                                MarketRatio = 0.330725M,
+                                                AuctionRatio = 0.192716m,
+                                                MarketRatio = 0.330725m,
                                             }
                                         },
                                         {
                                             "2010", new Year()
                                             {
-                                                AuctionRatio = 0.198498M,
-                                                MarketRatio = 0.363179M,
+                                                AuctionRatio = 0.198498m,
+                                                MarketRatio = 0.363179m,
                                             }
                                         },
                                         {
                                             "2011", new Year()
                                             {
-                                                AuctionRatio = 0.206337M,
-                                                MarketRatio = 0.374074M,
+                                                AuctionRatio = 0.206337m,
+                                                MarketRatio = 0.374074m,
                                             }
                                         },
                                         {
                                             "2012", new Year()
                                             {
-                                                AuctionRatio = 0.213178M,
-                                                MarketRatio = 0.431321M,
+                                                AuctionRatio = 0.213178m,
+                                                MarketRatio = 0.431321m,
                                             }
                                         }
                                     }
@@ -92,43 +92,43 @@
                             {
                                 Schedule = new Schedule()
                                 {
-                                    DefaultAuctionRatio = 0.06M,
-                                    DefaultMarketRatio = 0.06M,
+                                    DefaultAuctionRatio = 0.06m,
+                                    DefaultMarketRatio = 0.06m,
                                     Years = new Dictionary<string, Year>()
                                     {
                                         {
                                             "2016", new Year()
                                             {
-                                                AuctionRatio = 0.417468M,
-                                                MarketRatio = 0.613292M,
+                                                AuctionRatio = 0.417468m,
+                                                MarketRatio = 0.613292m,
                                             }
                                         },
                                         {
                                             "2017", new Year()
                                             {
-                                                AuctionRatio = 0.473205M,
-                                                MarketRatio = 0.692965M,
+                                                AuctionRatio = 0.473205m,
+                                                MarketRatio = 0.692965m,
                                             }
                                         },
                                         {
                                             "2018", new Year()
                                             {
-                                                AuctionRatio = 0.684991M,
-                                                MarketRatio = 0.980485M,
+                                                AuctionRatio = 0.684991m,
+                                                MarketRatio = 0.980485m,
                                             }
                                         },
                                         {
                                             "2019", new Year()
                                             {
-                                                AuctionRatio = 0.727636M,
-                                                MarketRatio = 1.041526M,
+                                                AuctionRatio = 0.727636m,
+                                                MarketRatio = 1.041526m,
                                             }
                                         },
                                         {
                                             "2020", new Year()
                                             {
-                                                AuctionRatio = 1.106366M,
-                                                MarketRatio = 0.772935M,
+                                                AuctionRatio = 1.106366m,
+                                                MarketRatio = 0.772935m,
                                             }
                                         }
                                     }
@@ -163,8 +163,8 @@
                     marketAuctionService.GetMarketAuctionItem(id);
 
                 bool itemWasNotFound = marketAuction.Equals(default(KeyValuePair<string, MarketAuction>));
-
-                System.Console.WriteLine($"Market Auction was not found: {itemWasNotFound.ToString()}");
+                
+                System.Console.WriteLine($"Market Auction found for ID: {id}");
                 Assert.False(itemWasNotFound);
             }
 
@@ -184,7 +184,7 @@
                     marketAuctionService.GetMarketAuctionYear(marketAuction, year);
                 bool itemWasNotFound = marketAuction.Equals(default(KeyValuePair<string, Year>));
 
-                System.Console.WriteLine($"Market Auction Year was not found: {itemWasNotFound.ToString()}");
+                System.Console.WriteLine($"Market Auction information found for year: {year}");
                 Assert.False(itemWasNotFound);
             }
 
@@ -198,6 +198,7 @@
 
                 if(itemWasNotFound)
                 {
+                    System.Console.WriteLine($"No information found for year {year} and ID {id}");
                     Assert.True(itemWasNotFound);
                 }
                 else
@@ -205,6 +206,7 @@
                     KeyValuePair<string, Year> marketAuctionYear = marketAuctionService.GetMarketAuctionYear(marketAuction, year);
                     MarketAuctionValue marketAuctionValue = marketAuctionService.GetMarketActionValue(id, year);
 
+                    System.Console.WriteLine($"Information found for year {year} and ID {id}");
                     System.Console.WriteLine($"MarketValue: {marketAuctionValue.MarketValue}");
                     System.Console.WriteLine($"AuctionValue: {marketAuctionValue.AuctionValue}");
                     Assert.True(marketAuctionValue.AuctionValue ==
@@ -212,18 +214,6 @@
                     Assert.True(marketAuctionValue.MarketValue ==
                         marketAuction.Value.SaleDetails.Cost * marketAuctionYear.Value.MarketRatio);
                 } 
-            }
-
-            [Theory]
-            [InlineData(2021, 87964)]
-            [InlineData(2020, 87964)]
-            public void ShouldGetNothingForInvalidIdAndYear(int year, int id)
-            {
-                MarketAuctionValue marketAuctionValue = marketAuctionService.GetMarketActionValue(id, year);
-                bool isNull = marketAuctionValue == null;
-
-                System.Console.WriteLine($"marketAuctionValue is null: {isNull}");
-                Assert.True(isNull);
             }
         }
     }
